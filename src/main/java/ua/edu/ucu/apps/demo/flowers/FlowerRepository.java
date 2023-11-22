40c695ffa72180f656e709770b397e1fa4859aa3
lizalaveikina <116518356+lizalaveikina@users.noreply.github.com>
2023-11-22 15:33:45 +0200

    Delete src directory

--- src/main/java/ua/edu/ucu/apps/demo/flowers/FlowerRepository.java
+++
@@ -1,7 +0,0 @@
-package ua.edu.ucu.apps.demo.flowers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlowerRepository extends JpaRepository<Flower, Integer> {
-
-}
